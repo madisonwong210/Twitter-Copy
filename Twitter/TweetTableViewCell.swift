3f241f22e768792c25f301154550b1f8caf11c97
madisonwong210 <58918746+madisonwong210@users.noreply.github.com>
2020-05-04 16:23:43 -0700

    favorite function done!!

--- Twitter/TweetTableViewCell.swift
+++ Twitter/TweetTableViewCell.swift
@@ -18,9 +18,37 @@ class TweetTableViewCell: UITableViewCell {
     @IBOutlet weak var retweetButtom: UIButton!
     
     @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Unfavorite didn't suceed: \(error)")
+            })
+        }
     }
     
     @IBAction func retweet(_ sender: Any) {
+    }
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
     }
     
     override func awakeFromNib() {
